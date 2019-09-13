50e99036d0fd225d6cd72af025a9107616051761
Amit Dave <amit.d@lendfoundry.com>
2019-09-14 03:26:33 +0530

    chore: Moved error to error.go

--- errors.go
+++ errors.go
@@ -7,3 +7,6 @@ var ErrNotImplemented = errors.New("Method / Function / Functionality no impleme
 
 // ErrAlreadyWatchingGivenKey :- Method / Function / Functionality no implemented
 var ErrAlreadyWatchingGivenKey = errors.New("Already watching given key")
+
+// ErrKeyNotFound :- the key does not exist
+var ErrKeyNotFound = errors.New("Key you are trying to access does not exist")
